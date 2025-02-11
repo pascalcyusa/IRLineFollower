c9abcb5625c2282684c090e8364aac2f4124abc6
pascalcyusa <shyakaadolf@gmail.com>
2025-02-11 14:33:02 -0500

    changed code for left_motor to work

--- main.py
+++ main.py
@@ -5,93 +5,57 @@ import time
 SENSOR_PIN1 = 8 
 SENSOR_PIN2 = 10
 
-# Motor 1 pins (Left)
-ENA = 12  
-IN1 = 24
-IN2 = 26
+# Set pin values
+ena1 = 12
+in1 = 24
+in2 = 26
 
-# Motor 2 pins (Right)
-ENB = 33
-IN3 = 11
-IN4 = 13
+ena2 = 32
+in3 = 11
+in4 = 13
 
-# Motor settings
-PWM_FREQ = 8000 
-BASE_SPEED = 50 
-TURN_SPEED = 30 
-
-
-# Setup GPIO
 GPIO.setmode(GPIO.BOARD)
 
 # Setup sensor pins
 GPIO.setup(SENSOR_PIN1, GPIO.IN)
 GPIO.setup(SENSOR_PIN2, GPIO.IN)
 
-# Setup motor 1 pins (Left)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
+# Board and pin setup
+GPIO.setup(ena1, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(ena2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
 
-# Setup motor 2 pins (Right)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
+# Set all pins low to start to prevent rotation on run
+GPIO.output(ena1, GPIO.LOW)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(ena2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
 
-
-# Initialize all motor control pins to LOW
-GPIO.output(IN1, GPIO.LOW)
-GPIO.output(IN2, GPIO.LOW)
-GPIO.output(IN3, GPIO.LOW)
-GPIO.output(IN4, GPIO.LOW)
-
-# Setup motor pins and PWM
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(ENB, GPIO.OUT)
-pwm_left = GPIO.PWM(ENA, PWM_FREQ)
-pwm_right = GPIO.PWM(ENB, PWM_FREQ)
-pwm_left.start(0)
-pwm_right.start(0)
+# Create PWM instance on enable pin A at 50 Hz
+motor1 = GPIO.PWM(ena1, 50)
+motor2 = GPIO.PWM(ena2, 50)
 
 def move_forward():
-    # Left motor forward
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    # Right motor forward
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    # Set speeds using PWM
-    pwm_left.ChangeDutyCycle(BASE_SPEED)
-    pwm_right.ChangeDutyCycle(BASE_SPEED)
+    motor1.start(20)
+    motor2.start(20)
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
 
-def turn_left():
-    # Left motor slower
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    # Right motor faster
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    # Set speeds using PWM
-    pwm_left.ChangeDutyCycle(TURN_SPEED)
-    pwm_right.ChangeDutyCycle(BASE_SPEED)
+# def turn_right():
 
-def turn_right():
-    # Left motor faster
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    # Right motor slower
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    # Set speeds using PWM
-    pwm_left.ChangeDutyCycle(BASE_SPEED)
-    pwm_right.ChangeDutyCycle(TURN_SPEED)
+     
 
-def stop():
-    # Stop both motors
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW)
-    pwm_left.ChangeDutyCycle(0)
-    pwm_right.ChangeDutyCycle(0)
+# def turn_left():
+     
+
+
 
 try:
     while True:
@@ -106,23 +70,26 @@ try:
         # Both sensors off line - stop
         elif left_sensor == GPIO.LOW and right_sensor == GPIO.LOW:
             print("No line detected - Stopping")
-            stop()
+            motor1.stop()
+            motor2.stop()
             
-        # Left sensor off line - turn right
-        elif left_sensor == GPIO.LOW:
-            print("Turn right")
-            turn_right()
+        # # Left sensor off line - turn right
+        # elif left_sensor == GPIO.LOW:
+        #     print("Turn right")
+        #     turn_right()
             
-        # Right sensor off line - turn left
-        elif right_sensor == GPIO.LOW:
-            print("Turn left")
-            turn_left()
+        # # Right sensor off line - turn left
+        # elif right_sensor == GPIO.LOW:
+        #     print("Turn left")
+        #     turn_left()
 
         time.sleep(0.1)
+       
 
 except KeyboardInterrupt:
-    # Clean up
-    stop()
-    pwm_left.stop()
-    pwm_right.stop()
+    # Stop motor
+    motor1.stop()
+    motor2.stop()
+    
+    # Cleanup pins
     GPIO.cleanup()
