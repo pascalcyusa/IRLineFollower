52f77d1e4401880f507859cc7f1b57511a1ebfe2
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2025-02-12 20:09:58 -0500

    final code that works on both tracks

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # IR Sensor pins
-SENSOR_PIN1 = 8 
+SENSOR_PIN1 = 8
 SENSOR_PIN2 = 10
 
 # Set pin values
@@ -28,7 +28,7 @@ GPIO.setup(ena2, GPIO.OUT)
 GPIO.setup(in3, GPIO.OUT)
 GPIO.setup(in4, GPIO.OUT)
 
-# Set all pins low to start to prevent rotation on run
+
 GPIO.output(ena1, GPIO.LOW)
 GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
@@ -36,13 +36,17 @@ GPIO.output(ena2, GPIO.LOW)
 GPIO.output(in3, GPIO.LOW)
 GPIO.output(in4, GPIO.LOW)
 
-# Create PWM instance on enable pin A at 50 Hz
+#14, 25 worked on track 2 decently well
+
+speed_1 = 14
+speed_2 = 25
+
 motor1 = GPIO.PWM(ena1, 50)
-motor2 = GPIO.PWM(ena2, 50)
+motor2 = GPIO.PWM(ena2, 100)
 
 def move_forward():
-    motor1.start(25)
-    motor2.start(25)
+    motor1.ChangeDutyCycle(speed_1)
+    motor2.ChangeDutyCycle(speed_2)
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
@@ -55,55 +59,55 @@ def stop():
     GPIO.output(in4, GPIO.LOW)
 
 def turn_right():
-    motor1.start(25)
-    motor2.start(25)
-    GPIO.output(in1, GPIO.HIGH)
-    GPIO.output(in2, GPIO.LOW)
-    GPIO.output(in3, GPIO.HIGH)
-    GPIO.output(in4, GPIO.LOW)
-
-     
-def turn_left():
-    motor1.start(25)
-    motor2.start(25)
+    motor1.start(speed_1)
+    motor2.start(speed_2)
     GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.HIGH)
      
+def turn_left():
+    motor1.start(speed_1)
+    motor2.start(speed_2)
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
 
 try:
+    motor1.start(0)
+    motor2.start(0)
     while True:
         left_sensor = GPIO.input(SENSOR_PIN2)
         right_sensor = GPIO.input(SENSOR_PIN1)
-        
+
         # Both sensors detect black line - move forward
-        if left_sensor == GPIO.HIGH and right_sensor == GPIO.HIGH:
+        if left_sensor == GPIO.LOW and right_sensor == GPIO.LOW:
             print("Black line detected - Moving forward")
             move_forward()
-        
+       
         # Both sensors off line - stop
-        elif left_sensor == GPIO.LOW and right_sensor == GPIO.LOW:
+        elif left_sensor == GPIO.HIGH and right_sensor == GPIO.HIGH:
             print("No line detected - Stopping")
             stop()
-            
+           
         # Left sensor off line - turn right
-        elif left_sensor == GPIO.LOW:
+        elif left_sensor == GPIO.HIGH:
             print("Turn right")
             turn_right()
-            
+           
         # Right sensor off line - turn left
-        elif right_sensor == GPIO.LOW:
+        elif right_sensor == GPIO.HIGH:
             print("Turn left")
             turn_left()
 
-        time.sleep(0.1)
+        time.sleep(0.01)
        
 
 except KeyboardInterrupt:
     # Stop motor
     motor1.stop()
     motor2.stop()
-    
+   
     # Cleanup pins
     GPIO.cleanup()
